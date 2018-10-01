5dca88aafea6429af992231c0c456b4596297041
Yevgeniya Terlyuk <43337986+down-dive@users.noreply.github.com>
2018-10-01 18:57:17 +0400

    uploading range.py

---
+++ range.py
@@ -0,0 +1,15 @@
+t = 8
+if t in range(1, 60):
+    print("It worked!")
+
+lets_try = [y for y in range(1,200)]
+print(lets_try)
+
+chetnie = [r for r in range(3,25) if r%2 == 0] #chetnie
+print(chetnie)
+
+nechetnie = [r for r in range(3,25) if r%2 != 0]
+print(nechetnie)
+
+q = [u for u in range(1,1000)]
+print(q)

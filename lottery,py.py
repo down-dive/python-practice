9635d212b77f6719cc14ba960e95489f0f015a46
Jane Terlyuk <jter1990@gmail.com>
2018-10-01 22:24:36 +0400

    adding .strip and .capitalize for lottery,py.py input

--- lottery,py.py
+++ lottery,py.py
@@ -1,12 +1,12 @@
 #lottery
 import random
 while True:
-    name = input("What is your name?:")
-    lets_play = input("Hello, {}! Would you like to play lottery?(y/n)".format(name))
+    name = input("What is your name?:").strip().capitalize()
+    lets_play = input("Hello, {}! Would you like to play lottery?(y/n)".format(name)).strip()
     if lets_play == "n":
         print("No problem! We can play another time.")
     else:
-        numbers = input("Great! Enter 5 digits please(1-10)")
+        numbers = input("Great! Enter 5 digits please(1-10)").strip()
 
         while numbers != (1,5,7,4,8):
               print("I'm sorry! Next time you'll get lucky!")

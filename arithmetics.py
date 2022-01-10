a5a2bce8518913d21da340dc5739602cae2c23cc
down-dive <yterlyuk@gmail.com>
2022-01-09 18:06:48 -0800

    arithmetics algorythms is complete

---
+++ arithmetics.py
@@ -0,0 +1,21 @@
+# Arithmetic
+
+# In this activity you will be writing code to create functions capable of different arithmetic operations.
+
+# Instructions
+
+#    - **add**: returns the result of `num1` plus `num2`.
+#   - **subtract**: returns the result of `num1` minus `num2`.
+#   - **multiply**: returns the result of `num1` times `num2`.
+#   - **divide**: returns the result of `num1` divided by `num2`.
+
+def add(num1, num2):
+    return num1 + num2
+
+print(add(234, 54325))
+
+def substract(num1, num2):
+    return num1 - num2
+
+print(substract(234, 54325))
+
